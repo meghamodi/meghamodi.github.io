29a33eea603da0660af18ed4a6d9e44e9931af0c
Megha Modi <meghamodi007@gmail.com>
2026-06-01 14:20:21 -0700

    fix in css

--- src/components/about/socials.jsx
+++ src/components/about/socials.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import {
-	faTwitter,
+	faXTwitter,
 	faGithub,
 	faLinkedin,
 } from "@fortawesome/free-brands-svg-icons";
@@ -18,7 +18,7 @@ const Socials = () => {
 				<a href={INFO.socials.twitter} target="_blank" rel="noreferrer">
 					<div className="social-icon">
 						<FontAwesomeIcon
-							icon={faTwitter}
+							icon={faXTwitter}
 							className="social-icon"
 						/>
 					</div>
--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -22,37 +22,10 @@ import myArticles from "../data/articles";
 import "./styles/homepage.css";
 
 const Homepage = () => {
-	// const [stayLogo, setStayLogo] = useState(false);
-	// const [logoSize, setLogoSize] = useState(80);
-	// const [oldLogoSize, setOldLogoSize] = useState(80);
 
 	useEffect(() => {
 		window.scrollTo(0, 0);
 	}, []);
-
-	// useEffect(() => {
-	// 	const handleScroll = () => {
-	// 		let scroll = Math.round(window.pageYOffset, 2);
-
-	// 		let newLogoSize = 80 - (scroll * 4) / 10;
-
-	// 		if (newLogoSize < oldLogoSize) {
-	// 			if (newLogoSize > 40) {
-	// 				setLogoSize(newLogoSize);
-	// 				setOldLogoSize(newLogoSize);
-	// 				setStayLogo(false);
-	// 			} else {
-	// 				setStayLogo(true);
-	// 			}
-	// 		} else {
-	// 			setLogoSize(newLogoSize);
-	// 			setStayLogo(false);
-	// 		}
-	// 	};
-
-	// 	window.addEventListener("scroll", handleScroll);
-	// 	return () => window.removeEventListener("scroll", handleScroll);
-	// }, [logoSize, oldLogoSize]);
 
 	const currentSEO = SEO.find((item) => item.page === "home");
 
